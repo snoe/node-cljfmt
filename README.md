2eddd0b87aaf89a1a5a785dbac17712ff5a7117a
Case Nelson <case@outpace.com>
2016-05-11 12:31:05 -0600

    Adding edn options file

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ A small cli utility that runs https://github.com/weavejester/cljfmt on a file.
 # Usage
 
 `cljfmt <filename>`
+`cljfmt --edn=<options.edn> <filename>`
+
+options.edn is an optional file containing a map of cljfmt options as laid out here: https://github.com/weavejester/cljfmt#configuration
+
--- src/node_cljfmt/main.cljs
+++ src/node_cljfmt/main.cljs
@@ -1,17 +1,40 @@
 (ns node-cljfmt.main
   (:require
    [cljs.nodejs :as nodejs]
-   [cljfmt.core :as cljfmt]))
+   [cljs.reader :as reader]
+   [cljfmt.core :as cljfmt]
+   [clojure.string :as string]))
 
 (nodejs/enable-util-print!)
 
 (def fs (nodejs/require "fs"))
 
+(defn parse-arg [parsed arg]
+  (if (.startsWith arg "--")
+    (let [[opt v] (string/split (subs arg 2) #"=" 2)]
+      (assoc-in parsed [:opts opt] v))
+    (assoc parsed :filename arg)))
+
+(defn parse-args [argv]
+  (loop [[arg & args] (drop 2 argv)
+         parsed {}]
+    (js/console.log "arg: " arg "args:" (pr-str args) "parsed: " (pr-str opts))
+    (if arg
+      (recur args (parse-arg parsed arg))
+      parsed)))
+
+(defn edn-opts [edn-filename]
+  (if edn-filename
+   (reader/read-string ((aget fs "readFileSync") edn-filename "utf8"))
+   {}))
+
 (defn -main []
-  (if-let [filename (nth (aget js/process "argv") 2)]
-    (let [file ((aget fs "readFileSync") filename "utf8")
-          formatted (cljfmt/reformat-string file)]
-      ((aget fs "writeFileSync") filename formatted "utf8"))
-    (js/console.error "Provide a filename")))
+  (let [parsed (parse-args (aget js/process "argv"))]
+    (if-let [filename (:filename parsed)]
+      (let [file ((aget fs "readFileSync") filename "utf8")
+            opts (edn-opts (get-in parsed [:opts "edn"]))
+            formatted (cljfmt/reformat-string file opts)]
+        ((aget fs "writeFileSync") filename formatted "utf8"))
+      (js/console.error "Provide a filename"))))
 
 (set! *main-cli-fn* -main)
